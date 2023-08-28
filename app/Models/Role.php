fa3ffe69381edde94502d52010d179e0912fdb4c
hoang <hoanghua216@gmail.com>
2023-08-28 15:00:02 +0700

    Accessors, Mutators and Scope

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Role extends Model
 {
     use HasFactory;
---
+++ app/Models/Scopes/ActiveScope.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class ActiveScope implements Scope
+{
+    // get active user
+    public function apply(Builder $builder, Model $model)
+    {
+		$builder->where('is_active', true); 
+    }
+}
