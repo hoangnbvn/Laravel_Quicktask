6fabc5abb119a61cf0a9dd102e8a7ca705d30556
hoang <hoanghua216@gmail.com>
2023-08-28 15:45:31 +0700

    create factories and seeders

---
+++ database/seeders/CreateInitialAdminAccount.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateInitialAdminAccount extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::unguard();
+
+		$admin = User::create(
+			[
+				'email' => 'admin.account@sun-asterisk.com',
+				'email_verified_at' => now(),
+				'password' => bcrypt('12345678'),
+				'first_name' => 'admin',
+				'last_name' => 'account',
+				'is_active' => true,
+				'is_admin' => true,
+				'username' => 'admin-account',
+			]
+		);
+    }
+}
